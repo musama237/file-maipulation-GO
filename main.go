436f20a25d6ac5e1b596e39c43a177046992194c
Muhammad Usama <usamamuhammad715@gmail.com>
2023-12-04 16:00:44 +0100

    main.go

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-func main() {
-	// main function
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	fileNames, err := parser() // Call the parser function
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the list of file names
+	fmt.Println("Files in the directory:")
+	for _, name := range fileNames {
+		fmt.Println(name)
+	}
+
+	rename(fileNames)
 }
