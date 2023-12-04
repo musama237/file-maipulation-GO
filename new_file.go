82a4985d59dbbfad25e6432f22a219e8f2bebe4c
Muhammad Usama <usamamuhammad715@gmail.com>
2023-12-05 00:41:22 +0100

    create-file.go

--- new_file.go
+++ new_file.go
@@ -1,5 +1,29 @@
 package main
 
-func new_file() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
+func new_file(directoryPath string) {
+
+	var fileName string
+	fmt.Println("Enter the name of new file ")
+	fmt.Scan(&fileName)
+	filePath := filepath.Join(directoryPath, fileName)
+
+	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
+		fmt.Println("The file alread exist in the directory")
+		return
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println("Error in creating file", err)
+		return
+
+	}
+	file.Close()
 }
