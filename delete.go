38cdfb47556f9f5d9fe7618cd7dab0ae392dbb28
Muhammad Usama <usamamuhammad715@gmail.com>
2023-12-05 01:49:38 +0100

    delete.go

--- delete.go
+++ delete.go
@@ -1,5 +1,23 @@
 package main
 
-func delete(fileNames []string) {
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func delete(directoryPath string) {
+
+	var fileName string
+	fmt.Println("Enter the name of new file ")
+	fmt.Scan(&fileName)
+	filePath := filepath.Join(directoryPath, fileName)
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Println("The file alread exist in the directory")
+		return
+	}
+
+	os.Remove(filePath)
 
 }
